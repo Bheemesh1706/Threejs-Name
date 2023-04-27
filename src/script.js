7dfb786cd984fccbd686d77167db418fa896741c
bheemesh1706 <bheemeshbala.71@gmail.com>
2023-04-28 01:03:35 +0530

    Changed name

--- src/script.js
+++ src/script.js
@@ -36,7 +36,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Bheemesh',
             {
                 font: font,
                 size: 0.5,
